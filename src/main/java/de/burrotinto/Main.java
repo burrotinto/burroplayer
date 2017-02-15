5d96d1d779afc0b402d2049f479bdc774f146826
Florian Klinger <derduke@posteo.de>
2017-02-15 14:08:52 +0100

    splashscreen

--- src/main/java/de/burrotinto/Main.java
+++ src/main/java/de/burrotinto/Main.java
@@ -3,10 +3,23 @@ package de.burrotinto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.util.Optional;
+
 @SpringBootApplication
-public class Main  {
+public class Main {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Main.class, args);
+        Optional.of(args[0]).ifPresent(s -> Main.startSplashscreen(s));
+        SpringApplication.run(Main.class, new String[0]);
+    }
+
+    public static Optional<Process> startSplashscreen(String spash) {
+        try {
+            return Optional.of(new ProcessBuilder("fbi", "-a", "-v", spash).start());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
     }
 }
