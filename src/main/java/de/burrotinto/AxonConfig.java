b40fa1ee77f249d41ea6fbe2f18d7b8bd5b66992
Florian <derduke@posteo.de>
2017-03-26 23:10:48 +0200

    Handle @enableAxon Bug

--- src/main/java/de/burrotinto/AxonConfig.java
+++ src/main/java/de/burrotinto/AxonConfig.java
@@ -3,6 +3,7 @@ package de.burrotinto;
 import de.burrotinto.comm.command.Serial;
 import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.SimpleCommandBus;
 import org.axonframework.commandhandling.model.Repository;
 import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventsourcing.EventSourcingRepository;
@@ -10,19 +11,20 @@ import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
-import org.axonframework.spring.config.EnableAxon;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by derduke on 02.03.17.
  */
-@EnableAxon
 @Configuration
 @RequiredArgsConstructor
 public class AxonConfig {
 
+    @Bean
+    public CommandBus commandBus() {
+        return new SimpleCommandBus();
+    }
 
     @Bean
     public EventStore eventStore() {
