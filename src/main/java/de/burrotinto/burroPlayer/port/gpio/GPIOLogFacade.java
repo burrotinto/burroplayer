106afb8a39531cd3b1cb0ea86ec15d00bd73962b
Florian <derduke@posteo.de>
2017-04-04 08:47:58 +0200

    GPIO log facade

---
+++ src/main/java/de/burrotinto/burroPlayer/port/gpio/GPIOLogFacade.java
@@ -0,0 +1,26 @@
+package de.burrotinto.burroPlayer.port.gpio;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by derduke on 27.03.17.
+ */
+@Slf4j
+@Service
+public class GPIOLogFacade implements GPIOFacade {
+    @Override
+    public void high(int gpio) {
+        log.info("GPIO Number:{} set HIGH",gpio);
+    }
+
+    @Override
+    public void low(int gpio) {
+        log.info("GPIO Number:{} set low",gpio);
+    }
+
+    @Override
+    public void blink(int gpio, long duration) {
+        log.info("GPIO Number:{} is BLINK for: {}",gpio,duration);
+    }
+}
--- src/main/java/de/burrotinto/burroPlayer/port/gpio/GPIOService.java
+++ src/main/java/de/burrotinto/burroPlayer/port/gpio/GPIOService.java
@@ -1,18 +1,39 @@
 package de.burrotinto.burroPlayer.port.gpio;
 
+
+import com.diozero.util.BoardInfo;
+import com.diozero.util.SystemInfo;
 import de.burrotinto.burroPlayer.port.gpio.pi4j.Pi4jGPIOFacade;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by derduke on 27.03.17.
  */
+@Slf4j
 @Primary
 @Service
 @RequiredArgsConstructor
-public class GPIOService implements GPIOFacade {
-    @Delegate(types = GPIOFacade.class)
-    private final Pi4jGPIOFacade facade;
+public class GPIOService implements GPIOFacade, InitializingBean {
+    private final GPIOLogFacade logFacade;
+    private final Pi4jGPIOFacade pi4jGPIOFacade;
+
+    @Delegate
+    private GPIOFacade facade;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        try {
+            BoardInfo boardInfo = SystemInfo.getBoardInfo();
+            log.info("{}", boardInfo);
+        } catch (Exception e) {
+            facade = logFacade;
+        }
+        log.info("{} GPIO Facade is used", facade);
+
+    }
 }
